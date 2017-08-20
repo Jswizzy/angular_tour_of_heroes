77bdb6b768a47922a28f1684d86d7e4455237e24
Jswizzy <smithjustinn@hotmail.com>
2017-08-20 18:41:08 -0400

    -init commit

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -5,8 +5,27 @@ import 'package:angular2/angular2.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '''
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div>
+    <label>id: </label>{{hero.id}}
+  </div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
+  ''',
+  directives: const [COMMON_DIRECTIVES],
 )
 class AppComponent {
-  var name = 'Angular';
+  String title = 'Tour of Heroes';
+  Hero hero = new Hero(1, 'Windstorm');
+}
+
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
 }
